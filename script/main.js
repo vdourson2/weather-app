a70f4047957b1786ba6e9e6d7a566ed0049689ef
Virginie Dourson <virginiedourson@gmail.com>
2023-10-11 17:16:31 +0200

    Refactor main.js

--- script/main.js
+++ script/main.js
@@ -8,26 +8,24 @@ getWeather()
     .then((d) => {
         let wrapForecast = document.querySelector('.wrapForecast');
         let listNb = 0;
-        for (let i=1; i<6; i++){
+        let limit = 3;
+        for (let j=1; j<6; j++){
 
             let day = document.createElement('div');
             day.classList.add('wrapForecast__day');
             wrapForecast.appendChild(day);
             
-            let limit = 3;
-            for (let j=1; j<9;j++){
-                let HourForecast = new HWeather(d.list[listNb],d.city.timezone);
-                if (HourForecast.hour >= limit) { //remplacer par un while
+            for (let h=1; h<9;h++){
+                let HourForecast = new HWeather(d.list[listNb++],d.city.timezone);
+                while (HourForecast.hour >= limit){
                     day.innerHTML += '<div class="wrapForecast__day__wrapHour"></div>';
+                    limit +=3;
+                    h++;
                 }
-                else {
-                    day.innerHTML += HourForecast.display();
-                    listNb++;
-                }
+                day.innerHTML += HourForecast.display();
                 limit +=3;
             }
         }
-        // console.log(new HWeather(d.list[0],d.city.timezone))
         
     });
 
