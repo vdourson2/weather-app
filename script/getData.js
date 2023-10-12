e4fb66dc9c8bc0d42985b928a7b882c8f5dd7b4f
Virginie Dourson <virginiedourson@gmail.com>
2023-10-12 16:14:37 +0200

    To deploy

--- script/getData.js
+++ script/getData.js
@@ -3,6 +3,7 @@ export async function getWeather(place) {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${place}&lang=fr&appid=${import.meta.env.VITE_KEY_WEATHERAPP}&units=metric`);
         const json = await response.json();
         //localStorage.setItem("data",JSON.stringify(json));
+        console.log(import.meta.env);
         console.log(json);
         return json;
 
