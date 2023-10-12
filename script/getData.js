d2ea1352762890b8963c2fc788d19fb3c06d34da
Virginie Dourson <virginiedourson@gmail.com>
2023-10-12 10:32:47 +0200

    Add form for the place

--- script/getData.js
+++ script/getData.js
@@ -1,12 +1,24 @@
-export async function getWeather() {
+// export async function getWeather(place) {
+//     try {
+//         const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${place}&lang=fr&appid=${import.meta.env.VITE_KEY_WEATHERAPP}&units=metric`);
+//         const json = await response.json();
+//         //localStorage.setItem("data",JSON.stringify(json));
+//         console.log(json);
+//         return json;
+
+//     }
+//     catch(error) {
+//         console.log('Erreur : impossible de récupérer les données')
+//     }
+// }
+
+
+//Function to get data from the local storage instead of the Openweather site
+export async function getWeather(place) {
     try {
-        console.log(import.meta.env);
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=london&lang=fr&appid=${import.meta.env.VITE_KEY_WEATHERAPP}&units=metric`);
-        const json = await response.json();
-        //localStorage.setItem("data",JSON.stringify(json));
+        let json = JSON.parse(localStorage.getItem("data"));
         console.log(json);
         return json;
-
     }
     catch(error) {
         console.log('Erreur : impossible de récupérer les données')
--- script/main.js
+++ script/main.js
@@ -1,32 +1,42 @@
-import '../style.css'
+import '../style.scss'
 // import javascriptLogo from './javascript.svg'
 // import viteLogo from '/vite.svg'
-import { getWeather } from '/script/getData.js'
+import { getWeather } from './getData.js'
 import { HWeather } from './hourWeather.js';
 
-getWeather()
-    .then((d) => {
+let input = document.querySelector('.header__input');
+input.addEventListener('keyup', (e) => {
+    if (e.code == "Enter") {
+        document.querySelector('.header__place').textContent = `Meteo in ${e.target.value}`;
         let wrapForecast = document.querySelector('.wrapForecast');
-        let listNb = 0;
-        let limit = 3;
-        for (let j=1; j<6; j++){
-
-            let day = document.createElement('div');
-            day.classList.add('wrapForecast__day');
-            wrapForecast.appendChild(day);
-            
-            for (let h=1; h<9;h++){
-                let HourForecast = new HWeather(d.list[listNb++],d.city.timezone);
-                while (HourForecast.hour >= limit){
-                    day.innerHTML += '<div class="wrapForecast__day__wrapHour"></div>';
-                    limit +=3;
-                    h++;
-                }
-                day.innerHTML += HourForecast.display();
-                limit +=3;
-            }
+        let wrapChildren = wrapForecast.children;//Faire une fonction séparée pour supprimer les éléments
+        while (wrapChildren.length>0) {
+            wrapForecast.removeChild(wrapChildren[0]);
         }
-        
-    });
+        getWeather(e.target.value).then((d) => {
+            e.target.value = '';
+            let listNb = 0;
+            let limit = 3;
+            for (let j=1; j<6; j++){
+
+                let day = document.createElement('div');
+                day.classList.add('wrapForecast__day');
+                wrapForecast.appendChild(day);
+                
+                for (let h=1; h<9;h++){
+                    let HourForecast = new HWeather(d.list[listNb++],d.city.timezone);
+                    while (HourForecast.hour >= limit){
+                        day.innerHTML += '<div class="wrapForecast__day__wrapHour"></div>';
+                        limit +=3;
+                        h++;
+                    }
+                    day.innerHTML += HourForecast.display();
+                    limit +=3;
+                }
+            }
+                
+        });
+    }
+})
 
 
